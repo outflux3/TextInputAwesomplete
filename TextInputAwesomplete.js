2aae5ca5db8770f73b0ee12f8c86898bd5aea634
outflux3 <marc@nibiri.com>
2019-05-26 10:09:34 -0400

    Initial commit

---
+++ TextInputAwesomplete.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+
+    $("input[data-enableawesomeplete=1]").each(function() {
+        initAwesomplete($(this));
+    });
+
+    $(document).on('reloaded opened repeateradd wiretabclick', '.InputfieldText', function() {
+        $(this).find("input[data-enableawesomeplete=1]").each(function() {
+        initAwesomplete($(this));
+        });
+    });
+});
+
+
+var initAwesomplete = function($textField) {
+    var thisID = $textField.attr('id');
+    var optsID = thisID.split('_repeater')[0];
+    var input = document.getElementById(thisID);
+    var awesomplete = new Awesomplete(input);
+}
+
